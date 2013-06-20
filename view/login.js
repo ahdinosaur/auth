e6d51d3fd22c58856873e541ff0b24fa615cf1e6
Michael Williams <dinosaur@riseup.net>
2013-06-19 22:05:14 -0700

    [auth] [view] added option to anchor dropdown to left or right, added option for custom button text, changed auth provider views

--- view/login.js
+++ view/login.js
@@ -6,11 +6,22 @@ module['exports'] = function(options, callback) {
       async = require('async'),
       auth = resource.use('auth');
 
+  // option to anchor dropdown to right or left
+  if (options.anchor === 'left') {
+    // defaults to left
+  } else if (options.anchor === 'right') {
+    $('#authDropdown').addClass('dropdown-anchor-right');
+  }
+
+  if (options.text) {
+    $('#authButton').text(options.text);
+  }
+
   // for each auth provider
   async.each(Object.keys(auth.providers),
     // append provider login to dropdown
     function(providerName, callback) {
-      auth.providers[providerName].view.login.min.present(options, function(err, result) {
+      auth.providers[providerName].view.login.present(options, function(err, result) {
         if (err) { return callback(err); }
         $('#authProviders').append("<li>"+result+"</li>");
         return callback(null);
